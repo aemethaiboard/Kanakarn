544ebb4c11bf78baff0d1ad623f76162a6924330
aemethaiboard <41520398+aemethaiboard@users.noreply.github.com>
2018-07-22 02:08:02 +0700

    Update bot_settings.php

--- bot_settings.php
+++ bot_settings.php
@@ -1,6 +1,6 @@
 <?php
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID','กรอก ค่า Channel ID');
-define('LINE_MESSAGE_CHANNEL_SECRET','กรอกค่า Channel secret');
-define('LINE_MESSAGE_ACCESS_TOKEN','กรอกค่า Channel access token');
+define('LINE_MESSAGE_CHANNEL_ID','1595764194');
+define('LINE_MESSAGE_CHANNEL_SECRET','9dcea8f6ee7c47ab9523ef00474ca0ee');
+define('LINE_MESSAGE_ACCESS_TOKEN','iFGXK2ZblmbslB+r/NhqXriLHWTxsPT1PxCYUgmn4+J9xoF/amkR632zfLXngtEmNkPBTdYECMPZ/RswvU9uLhGVryp4nUT0FtK+3ovyafkEETcxy7AxG+HVu3AwUgQoVffperEeuqQmOkn4n9YCVQdB04t89/1O/w1cDnyilFU=');
 ?>
