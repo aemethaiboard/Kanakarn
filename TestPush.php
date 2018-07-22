c390782182a4e5465b5c5e292de03a48662f2219
aemethaiboard <41520398+aemethaiboard@users.noreply.github.com>
2018-07-22 15:56:32 +0700

    Update TestPush.php

--- TestPush.php
+++ TestPush.php
@@ -1,9 +1,9 @@
 <?php
  
 $strAccessToken = "A7fyuqaArDj1uvQUhcTpVaLm7r8PGppXWbWja/JH8cGC+qupCOHwrqgxQ2Ja6CvKNkPBTdYECMPZ/RswvU9uLhGVryp4nUT0FtK+3ovyafkzxkryhpNoafwRY0RbZKEnI1qCHNI0YAtjr6q0/I9BSwdB04t89/1O/w1cDnyilFU=";
- 
 $strUrl = "https://api.line.me/v2/bot/message/push";
- 
+$group = client.getGroupByName('Test Bot Line')
+
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
@@ -11,8 +11,8 @@ $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 $arrPostData = array();
 $arrPostData['to'] = "U9d4db29b76910af5a6679e56a3a96172";
 $arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
- 
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message $group";
+
  
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
