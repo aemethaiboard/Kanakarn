7ea4a76cc93d48e4a0ea57c97c70c11c9630b1f9
aemethaiboard <41520398+aemethaiboard@users.noreply.github.com>
2018-07-22 16:51:27 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,6 +5,18 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'A7fyuqaArDj1uvQUhcTpVaLm7r8PGppXWbWja/JH8cGC+qupCOHwrqgxQ2Ja6CvKNkPBTdYECMPZ/RswvU9uLhGVryp4nUT0FtK+3ovyafkzxkryhpNoafwRY0RbZKEnI1qCHNI0YAtjr6q0/I9BSwdB04t89/1O/w1cDnyilFU=';
 
+$groupId = NULL;
+$roomId = NULL;
+
+// สร้างตัวแปรเก็บค่า groupId กรณีเป็น Event ที่เกิดขึ้นใน GROUP
+if($eventObj->isGroupEvent()){
+    $groupId = $eventObj->getGroupId();  
+}
+// สร้างตัวแปรเก็บค่า roomId กรณีเป็น Event ที่เกิดขึ้นใน ROOM
+if($eventObj->isRoomEvent()){
+    $roomId = $eventObj->getRoomId();            
+}
+
 // Get POST body content
 $content = file_get_contents('php://input');
 
@@ -24,7 +36,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text,$groupId
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -39,13 +51,13 @@ if (!is_null($events['events'])) {
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post,$groupId);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-			echo $result . "\r\n";
+			echo $result,$groupId . "\r\n";
 		}
 	}
 }
