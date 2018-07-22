6ad95550b41f33a19e28fae3de0a44e8f5d59ca1
aemethaiboard <41520398+aemethaiboard@users.noreply.github.com>
2018-07-22 17:47:12 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,8 +8,8 @@ $access_token = 'A7fyuqaArDj1uvQUhcTpVaLm7r8PGppXWbWja/JH8cGC+qupCOHwrqgxQ2Ja6Cv
 
 $channelSecret = '1ff4cb7f8474791abfb5381a833a2973';
 
-//$pushID = 'U9d4db29b76910af5a6679e56a3a96172';
-$pushID = '1cb155bc-e8bf-47e1-8bbb-3b49e4ffc7ad';
+$pushID = 'U9d4db29b76910af5a6679e56a3a96172';
+//$pushID = '1cb155bc-e8bf-47e1-8bbb-3b49e4ffc7ad';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
